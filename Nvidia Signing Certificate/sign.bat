d2aad703abf2f938c2f15adf642cc208b8e24647
De-eloper/theghost <113988354+De-eloper@users.noreply.github.com>
2022-11-02 16:17:17 +0000

    Add files via upload

--- Nvidia Signing Certificate/sign.bat
+++ Nvidia Signing Certificate/sign.bat
@@ -2,10 +2,14 @@
 
 cd %~dp0
 
-goto sign
+@if "x%1" EQU "x" (goto help) else (goto sign)
+:help
+@echo Usage: sign.bat filename
+@echo Note:  Original file will be overrided by the signed one, no backup
+@goto :end
 :sign
 
-@echo Sign File King.dll
+@echo Sign File %1
 set message=%DATE%
 date 01.03.2014
 signtool.exe sign /v /ac 1111222.cer /f current_cert.pfx /p nv1d1aRules %1
